09ad9ed5c5126c346951472d93775c4c221afd49
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-04-14 10:23:53 +0300

    start

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+// import image from 'img/no-image.jpg';
+
+export const GlobalStyle = createGlobalStyle`
+
+html {
+  height: 100%;
+}
+
+#root {
+  height: 100%;
+  position: relative;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* background-color: #d9ebe9; */
+}
+
+main {
+  margin-top: 45px;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+
+a {
+  text-decoration: none;
+}
+
+li {
+  list-style: none;
+}
+
+ul {
+  padding: 0;
+}
+`;
